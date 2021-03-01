3ad8968851bb09839d300de2f892b67b926f95c3
Sergey Petrelevich <petrelevich@yandex.ru>
2021-03-02 01:11:34 +0300

    fixed index test

--- src/test/java/com/example/javaspringblog/controllers/HomeControllerTests.java
+++ src/test/java/com/example/javaspringblog/controllers/HomeControllerTests.java
@@ -4,24 +4,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
-// import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-// @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HomeControllerTests {
+class HomeControllerTests {
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  void index() throws Exception {
+    @Test
+    void index() throws Exception {
 
-    mockMvc.perform(post("/")).andExpect(status().isOk());
-  }
+        mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
 }
