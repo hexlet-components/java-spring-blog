9e8314d4a9415ab48ef63c808d3700aeccf59471
Kirill Mokevnin <mokevnin@gmail.com>
2021-03-07 08:18:05 -0500

    fix liquibase configuration
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- src/main/java/com/example/javaspringblog/models/Post.java
+++ src/main/java/com/example/javaspringblog/models/Post.java
@@ -7,14 +7,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
