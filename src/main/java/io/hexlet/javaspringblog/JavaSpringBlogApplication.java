a700bb191edf46c1358b7c3537e453b9cf34ee5d
velya <velya.96@gmail.com>
2021-11-03 19:07:01 +0300

    Fixed name of _main_ class

--- src/main/java/io/hexlet/javaspringblog/JavaSpringBlogApplication.java
+++ src/main/java/io/hexlet/javaspringblog/JavaSpringBlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaspringblogApplication {
+public class JavaSpringBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavaspringblogApplication.class, args);
+        SpringApplication.run(JavaSpringBlogApplication.class, args);
     }
 
 }
