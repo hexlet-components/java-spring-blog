e8d22248d5e97160d1a0312ac0ad9d3c1008390d
Ivan Gagarinov <dzencot@gmail.com>
2022-03-29 15:31:27 +0500

    fix lint

--- src/main/java/io/hexlet/javaspringblog/config/rollbar/RollbarConfig.java
+++ src/main/java/io/hexlet/javaspringblog/config/rollbar/RollbarConfig.java
@@ -6,7 +6,6 @@ import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.beans.factory.annotation.Value;
 
 
