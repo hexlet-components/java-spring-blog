d956b8840e0f09969f0944b1a69b42f7669d2c28
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-04-18 16:08:26 +0300

    it works

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into users (first_name, last_name, email, password, created_at) values ('Retha', 'Astupenas', 'rastupenas0@mysql.com', '12345', '2022-01-01 00:00:00');
+-- insert into users (id, first_name, last_name, email, password, created_at) values (2, 'Chandra', 'Eck', 'ceck1@oracle.com', '12345', '2022-01-01 00:00:00');
+-- insert into users (id, first_name, last_name, email, password, created_at) values (3, 'Sybila', 'Tomasino', 'stomasino2@lulu.com', '12345','2022-01-01 00:00:00');
+-- insert into users (id, first_name, last_name, email, password) values (4, 'Jacquelynn', 'Viner', 'jviner3@webeden.co.uk', '12345');
+-- insert into users (id, first_name, last_name, email, password) values (5, 'Olivia', 'Spyvye', 'ospyvye4@ucsd.edu', '12345');
+
+-- insert into posts (id, author_id, title, body) values (1, 1, 'My thoughts', 'Most of the world`s biggest problems are because of somebody trying to find a simple answer.');
+-- insert into posts (id, author_id, title, body) values (2, 2, 'My first post', 'Feed a man a fish, feed him for a night.Teach a man to fish, feed him for a lifetime.Teach a man to reason from first principles, he can teach himself to fish. ');
+-- insert into posts (id, author_id, title, body) values (3, 3, 'Perfectionism', 'Perfectionism is not a quest for the best. It is a pursuit of the worst in ourselves, the part that tells us that nothing we do will ever be good enough-that we should try again.');
+-- insert into posts (id, author_id, title, body) values (4, 4, 'Control', 'When circumstances aren`t ideal, don`t assume you have no control. You always do. So instead of feeling bad that you can`t do everything, do something!');
+-- insert into posts (id, author_id, title, body) values (5, 5, 'My great post', 'If you do not change direction, you might end up where you are heading.');
+
+-- insert into post_comments (id, author_id, post_id, body) values (1, 1, 5, 'Great!');
+-- insert into post_comments (id, author_id, post_id, body) values (2, 2, 3, 'Awesome!');
+-- insert into post_comments (id, author_id, post_id, body) values (3, 3, 4, 'So so');
+-- insert into post_comments (id, author_id, post_id, body) values (4, 4, 3, 'Rubish');
+-- insert into post_comments (id, author_id, post_id, body) values (5, 5, 1, 'I dont agree');
