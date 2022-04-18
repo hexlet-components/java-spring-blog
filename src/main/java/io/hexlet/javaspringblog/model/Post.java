d0a4641610b762a67320e34c8119293bfeee8448
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-04-18 16:42:31 +0300

    add init data to database

--- src/main/java/io/hexlet/javaspringblog/model/Post.java
+++ src/main/java/io/hexlet/javaspringblog/model/Post.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import static javax.persistence.GenerationType.AUTO;
+import static javax.persistence.GenerationType.IDENTITY;
 import static javax.persistence.TemporalType.TIMESTAMP;
 
 @Entity
@@ -30,7 +30,7 @@ import static javax.persistence.TemporalType.TIMESTAMP;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = AUTO)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @ManyToOne
