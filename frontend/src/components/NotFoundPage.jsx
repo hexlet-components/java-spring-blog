0760212ff4d578048fc0d144735fc64373dcc0c5
Ivan Gagarinov <dzencot@gmail.com>
2022-06-09 19:40:03 +0500

    fix

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
 
   return (
     <div className="text-center">
-      <img alt={t('notFound.header')} className="img-fluid h-25" src="src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg"" />
+      <img alt={t('notFound.header')} className="img-fluid h-25" src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
       <h1 className="h4 text-muted">
         {t('notFound.header')}
       </h1>
