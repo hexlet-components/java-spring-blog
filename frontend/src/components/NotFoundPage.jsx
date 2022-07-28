4af031ced05746ea8fdc47d5a77a5405780890f8
Ivan Gagarinov <dzencot@gmail.com>
2022-07-28 13:28:15 +0500

    fix 404 image

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+// import ImageNotFound from '../../public/404.svg';
 
 const NotFoundPage = () => {
   const { t } = useTranslation();
 
   return (
     <div className="text-center">
-      <img alt={t('notFound.header')} className="img-fluid h-25" src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
+      <img alt={t('notFound.header')} style={{ width: '495px' }} className="img-fluid h-25" src="404.svg" />
       <h1 className="h4 text-muted">
         {t('notFound.header')}
       </h1>
