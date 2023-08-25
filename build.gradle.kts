cb2c2c0148c650039bf47978514cb1a933054e2f
Kirill Mokevnin <mokevnin@gmail.com>
2023-08-25 13:22:44 -0400

    fix indentation
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-  application
-  // "checkstyle"
-  id("io.freefair.lombok") version "8.2.2"
-  id("org.springframework.boot") version "3.1.3"
-  id("io.spring.dependency-management") version "1.1.3"
-  id("com.github.ben-manes.versions") version "0.47.0"
+    application
+    // "checkstyle"
+    id("io.freefair.lombok") version "8.2.2"
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("com.github.ben-manes.versions") version "0.47.0"
 }
 
 group = "io.hexlet.blog"
@@ -18,40 +18,40 @@ application { mainClass.set("io.hexlet.blog.Application") }
 repositories { mavenCentral() }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-validation")
-  implementation("org.springframework.boot:spring-boot-devtools")
-  implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-configuration-processor")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springframework.security:spring-security-test")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("org.springframework.security:spring-security-test")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
-  implementation("org.modelmapper:modelmapper:3.1.1")
+    implementation("org.modelmapper:modelmapper:3.1.1")
 
-  implementation("org.instancio:instancio-junit:3.2.0")
-  implementation("net.javacrumbs.json-unit:json-unit-assertj:3.0.0")
-  implementation("net.datafaker:datafaker:2.0.1")
+    implementation("org.instancio:instancio-junit:3.2.0")
+    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.0.0")
+    implementation("net.datafaker:datafaker:2.0.1")
 
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.28.0")
-  runtimeOnly("com.h2database:h2:2.2.220")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation(platform("org.junit:junit-bom:5.10.0"))
-  testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.28.0")
+    runtimeOnly("com.h2database:h2:2.2.220")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 
 }
 
 tasks.test {
-  useJUnitPlatform()
-  // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
-  testLogging {
-    exceptionFormat = TestExceptionFormat.FULL
-    events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-    // showStackTraces = true
-    // showCauses = true
-    showStandardStreams = true
-  }
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
