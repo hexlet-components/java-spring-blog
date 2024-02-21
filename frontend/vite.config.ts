6da40431761ab1b4d70c4ba402bd19a80279425f
Ivan Gagarinov <dzencot@gmail.com>
2024-02-21 18:14:39 +0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-      host: true,
-      proxy: {
-          '/api': {
-              target: 'http://localhost:8080'
-          }
-      }
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+      },
+    },
   },
 });
