c260de62d3b13cbb28faad1546084a6471c8f748
Kirill Mokevnin <mokevnin@gmail.com>
2025-05-23 17:43:13 -0400

    update settings
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "HexletSpringBlog"
 // spring.jpa.hibernate.ddl-auto
 
 plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.60.5"
 }
